2a49a273351a4dc509b97317653e8d42caaff2c2
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-07 15:56:09 -0700

    implement oauth flow including handling refresh tokens

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 8888;
 const querystring = require('querystring');
+const axios = require('axios');
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
@@ -28,25 +29,97 @@ const generateRandomString = length => {
 
 const stateKey = 'spotify_auth_state';
 
+// request authorization to access data from Spotify by sending a get request (redirect to) 
 app.get('/login', (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   const scope = 'user-read-private user-read-email';
 
-  const queryParans = querystring.stringify({
+  const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
     response_type: 'code',
     redirect_uri: REDIRECT_URI,
     scope: scope,
     state: state
   })
-  res.redirect(`https://accounts.spotify.com/authorize?${queryParans}`);
+
+  // authorizes access, then prompts user to log in to Spotify, user is redirected to REDIRECT_URI
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
 app.get('/callback', (req, res) => {
-  res.send('Callback');
-})
+  // authorization code included in request query params in stored inside 'code' variable
+  const code = req.query.code || null;
+
+  // use authorization code to request access token by sending a post request to /api/token
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+  // spotify confirms authorization code validity, then responds with access token and refresh token
+  .then(response => {
+    if (response.status === 200) {
+      const { access_token, token_type } = response.data;
+
+      // use access token to request data from Spotify API
+      // axios.get('https://api.spotify.com/v1/me', {
+      //   headers: {
+      //     Authorization: `${token_type} ${access_token}`
+      //   }
+      // })
+      const { refresh_token } = response.data;
+
+      axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
+        .then(response => {
+          res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        })
+        .catch(error => {
+          res.send(error);
+        });
+    } else {
+      res.send(response);
+    }
+  })
+  .catch(error => {
+    res.send(error);
+  });
+});
+
+// use the refresh_token to retrieve another access token once current token expires (so user doesn't have to login every 60 mins)
+app.get('/refresh_token', (req, res) => {
+  const { refresh_token } = req.query;
+
+  // refresh token included in request query params > Spotify confirms validty of refresh token, then response with new access token
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
