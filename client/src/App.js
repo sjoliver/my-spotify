164c857034f6792184947696b3c84f822a2c585d
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-07 16:30:59 -0700

    implement useEffect to grab access and refresh tokens from query string

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // grabs the query string (everything after the '?')
+    const queryString = window.location.search;
+
+    // creates & returns a new URLSearchParams object which contains utility methods like .get()
+    const urlParams = new URLSearchParams(queryString);
+
+    // grab the access token and refresh tokens from the query string
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    console.log('access:', accessToken);
+    console.log('refresh:', refreshToken);
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
         </a>
       </header>
     </div>
