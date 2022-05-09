c963d6c71e19a19e59b597ec3978e8001d3ec7c8
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-08 18:04:18 -0700

    add hasTokenExpired fn and async refreshToken fn to make an API call to the /refresh_token endpoint & to set the accessToken with the response

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // Map for localStorage keys
 const LOCALSTORAGE_KEYS = {
   accessToken: 'spotify_access_token',
@@ -13,6 +15,48 @@ const LOCALSTORAGE_VALUES = {
   expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
   timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
 };
+
+const hasTokenExpired = () => {
+  const { accessToken, timestamp, expireTime } = LOCALSTORAGE_VALUES;
+
+  if (!accessToken || !timestamp) {
+    return false;
+  }
+
+  const millisecondsElapsed = Date.now() - Number(timestamp);
+
+  // checks if amount of time elapsed since the timestamp is greater than the access token's expire time (3600 seconds)
+  return (millisecondsElapsed / 1000) > Number(expireTime);
+}
+
+// use refresh token in localStorage to hit the /refresh_token endpoint,
+// then update the values in localStorage with data from the response
+// Using async due to API call to /refresh_token endpoint
+const refreshToken = async () => {
+  try {
+    // Logout if there's no refresh token stored or we've managed to get into a reload infinite loop
+    if (!LOCALSTORAGE_VALUES.refreshToken ||
+      LOCALSTORAGE_VALUES.refreshToken === 'undefined' ||
+      (Date.now() - Number(LOCALSTORAGE_VALUES.timestamp) / 1000) < 1000
+    ) {
+      console.error('No refresh token available');
+      logout();
+    }
+
+    // Use `/refresh_token` endpoint from our Node app
+    const { data } = await axios.get(`/refresh_token?refresh_token=${LOCALSTORAGE_VALUES.refreshToken}`);
+
+    // Update localStorage values
+    window.localStorage.setItem(LOCALSTORAGE_KEYS.accessToken, data.access_token);
+    window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+
+    // Reload the page for localStorage updates to be reflected
+    window.location.reload();
+
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 // Handles logic for retrieving the Spotify access token from localStorage or URL query params 
 // returns {string} a Spotify access token
