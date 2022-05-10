7f25115a0a85c6c8d1ca13aaa0b5dc8f9a5e190d
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-10 12:46:25 -0700

    add link to Playlists page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,8 @@ import { getCurrentUserProfile } from '../spotify';
 const Profile = () => {
   const [profile, setProfile] = useState(null);
 
+  console.log(profile)
+
   useEffect(() => {
     // getCurrentUserProfile returns a promise, we must use await to wait for the promise to be resoled -- we handle this by creating an async fn 
     const fetchData = async () => {
@@ -24,6 +26,7 @@ const Profile = () => {
         <div>
           <h1>{profile.display_name}</h1>
           <p>{profile.followers.total} Followers</p>
+          <a href="/playlists">Playlists</a>
           {profile.images.length && profile.images[0].url && (
             <img src={profile.images[0].url} alt="Avatar"/>
           )}
