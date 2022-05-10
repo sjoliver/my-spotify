67d75e0a452173ebd9699583104b0160b9df0ae1
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-10 11:54:14 -0700

    new styled component for logout button, add button app-wide

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-// import './App.css';
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { accessToken, logout, getCurrentUserProfile } from './spotify';
-import { catchErrors } from './utils';
-import { Login } from './pages';
+import { accessToken, logout } from './spotify';
+import { Login, Profile, Playlists } from './pages';
 import { GlobalStyle } from './styles';
+import styled from 'styled-components/macro';
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
 
 // scroll to top of page when changing routes
 function ScrollToTop() {
@@ -20,24 +35,29 @@ function ScrollToTop() {
 function App() {
   const [token, setToken] = useState(null);
 
+  useEffect(() => {
+    setToken(accessToken)
+  }, []);
+
   return (
     <div className="App">
       <GlobalStyle />
 
-      <header className="App-header">
         {!token ? (
           <Login />
         ) : (
-          <Router>
-            <ScrollToTop />
-            <Routes>
-              <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
-              <Route path="/playlists" element={<h1>Playlists</h1>} />
-              <Route path="/" element={<button onClick={logout}>Log Out</button>} />
-            </Routes>
-          </Router>
+          <>
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+            <Router>
+              <ScrollToTop />
+              <Routes>
+                <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+                <Route path="/playlists" element={<Playlists />} />
+                <Route path="/" element={<Profile />} />
+              </Routes>
+            </Router>
+          </>
         )}
-      </header>
     </div>
   );
 }
