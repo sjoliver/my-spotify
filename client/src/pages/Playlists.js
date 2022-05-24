e3fe4d88868a84dbff4b0673ee2645531e01fe2b
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-24 14:36:19 -0700

    refactor topSongs logic into own component, clean up playlists component

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
+import TopSongs from './TopSongs';
 import { catchErrors } from '../utils';
 import { getCurrentUserPlaylists } from '../spotify';
 
@@ -10,62 +10,9 @@ import Checkbox from '@mui/material/Checkbox';
 
 const Playlists = () => {
   const [playlists, setPlaylists] = useState([]);
-  const [topSongs, setTopSongs] = useState([]);
-  
-  let playlistsList = [];
-  let playlistID = [];
-  let allTracks = {};
-  let options;
-  
-  if (playlists.length > 1) {
-    // create array of playlist ids
-    for (let playlist of playlists.items) {
-      playlistID.push(playlist.id)
-    }
-
-    options = playlists.items.map(playlist => playlist.name)
-  }
-  
-  // create array equal to the length of the number of checkboxes using the array "fill" method
-  const [checkedState, setCheckedState] = useState(
-    playlists.length > 1 ? new Array(options.length).fill(false) : []
-  );
-
-  // create array of playlistEndpoints using their IDs
-  let playlistEndpoints = playlistID.map(id => `/playlists/${id}`);
-
-  // concurrently make an axios get requests for each endpoint 
-  const getTracksTEST = async () => {
-    try {
-      const playlists = await axios.all(playlistEndpoints.map((endpoint) => axios.get(endpoint)))
-      
-      // loop through each playlist
-      for (let playlist of playlists) {
-
-        // playlist.data.tracks.items = array of track objects
-        let trackObj = playlist.data.tracks.items; 
-
-        for (const song of trackObj) {
-          if (song.track.id in allTracks) {
-            allTracks[`${song.track.id}`][1] += 1
-          } else {
-            allTracks[`${song.track.id}`] = [song.track.name, 1]
-          }
-        }
-      }
-      
-      const allTracksArr = Object.values(allTracks);
-      const sortedTracks = allTracksArr.sort((a, b) => b[1] - a[1]);
-
-      setTopSongs(sortedTracks.slice(0,20));
-
-    } catch(err) {
-      console.log(err)
-    }
-  }
 
   useEffect(() => {
-    // getCurrentUserPlaylists returns a promise, we must use await to wait for the promise to be resoled -- we handle this by creating an async fn 
+    // getCurrentUserPlaylists returns a promise, we must use await to wait for the promise to be resolved -- we handle this by creating an async fn 
     const fetchData = async () => {
       const { data } = await getCurrentUserPlaylists();
 
@@ -75,49 +22,20 @@ const Playlists = () => {
     // handles errors from async fn 
     catchErrors(fetchData());
 
-    getTracksTEST();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleChange = (event) => {
-    const updatedCheckedState = checkedState.map((item, index) => 
-      index === event ? !item : item
-    );
-
-    setCheckedState(updatedCheckedState);
-  }
-
-  if (playlists.length > 1) {
-    // create array of playlist names
-    playlistsList = playlists.items.map((playlist, index) => {
-      return (
-        <div key={index}>
-          <FormControlLabel 
-            control={<Checkbox checked={checkedState[index]} onChange={() => handleChange(index)}/>} 
-            label={`${playlist.name}`} 
-          />
-        </div>
-      )
-    })
-  }
-
-  let topSongsList;
-
-  if (topSongs.length > 1) {
-    // create array of playlist names
-    topSongsList = topSongs.map((topSong, index) => {
-      return <li key={index}>{topSong[0]}: {topSong[1]}</li>
-    });
-  };
+  console.log("PLAYLISTS", playlists)
 
   return (
     <>
       <FormGroup>
-        {playlistsList}
+        {playlists?.items.map((playlist, index) => {
+          return (
+            <FormControlLabel control={<Checkbox />} label={`${playlist.name}`} key={index} />
+        )
+        })}
       </FormGroup>
-      <ol>
-        {topSongsList}
-      </ol>
+      <TopSongs playlists={playlists} />
     </>
   )
 
--- client/src/pages/TopSongs.js
+++ client/src/pages/TopSongs.js
@@ -1,9 +1,78 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const TopSongs = (props) => {
   const [topSongs, setTopSongs] = useState([]);
+  const {playlists} = props
+
+  let playlistID = [];
+  let allTracks = {};
+  
+  // create array of playlist ids
+  if (playlists.length > 1) {
+    for (let playlist of playlists.items) {
+      playlistID.push(playlist.id)
+    }
+  }
+
+  // create array of playlist endpoints using their IDs
+  let playlistEndpoints = playlistID.map(id => `/playlists/${id}`);
+
+  // concurrent GET requests using axios for each endpoint 
+  const getTracks = async () => {
+    try {
+      const playlists = await axios.all(playlistEndpoints.map((endpoint) => axios.get(endpoint)))
+      
+      // loop through each playlist
+      for (let playlist of playlists) {
+
+        // playlist.data.tracks.items = array of track objects
+        let trackObj = playlist.data.tracks.items; 
+
+        for (const song of trackObj) {
+          if (song.track.id in allTracks) {
+            allTracks[`${song.track.id}`][1] += 1
+          } else {
+            allTracks[`${song.track.id}`] = [song.track.name, 1]
+          }
+        }
+      }
+      
+      const allTracksArr = Object.values(allTracks);
+      const sortedTracks = allTracksArr.sort((a, b) => b[1] - a[1]);
+
+      setTopSongs(sortedTracks.slice(0,20));
+
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getTracks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let topSongsList;
+
+  if (playlists) {
+
+     // LIST OF TOP SONGS
+     topSongsList = topSongs.map((topSong, index) => {
+      return <li key={index}>{topSong[0]}: {topSong[1]}</li>
+    });
+
+    console.log("top songs", topSongsList)
+  }
 
 
+  return (
+    <>
+      <ol>
+        {topSongsList}
+      </ol>
+    </>
+  )
 
 
 }
