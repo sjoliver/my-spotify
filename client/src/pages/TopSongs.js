95d0413860d00ce7fae4882fcafc56c174426b8e
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-28 16:27:49 -0700

    commenting component out for now

--- client/src/pages/TopSongs.js
+++ client/src/pages/TopSongs.js
@@ -1,79 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { catchErrors } from '../utils';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { catchErrors } from '../utils';
 
-const TopSongs = (props) => {
-  const [topSongs, setTopSongs] = useState([]);
-  const {playlists} = props;
+// const TopSongs = (props) => {
+//   const [topSongs, setTopSongs] = useState([]);
+//   const {playlists} = props;
 
-  let playlistID = [];
+//   let playlistID = [];
   
-  // create array of playlist ids
-  if (playlists) {
-    for (let playlist of playlists.items) {
-      playlistID.push(playlist.id)
-    }
-  }
+//   // create array of playlist ids
+//   if (playlists) {
+//     for (let playlist of playlists.items) {
+//       playlistID.push(playlist.id)
+//     }
+//   }
 
-  // create array of playlist endpoints using their IDs
-  let playlistEndpoints = playlistID.map(id => `/playlists/${id}`);
-
+//   // create array of playlist endpoints using their IDs
+//   let playlistEndpoints = playlistID.map(id => `/playlists/${id}`);
   
-  useEffect(() => {
-    // console.log("hey")
+//   useEffect(() => {
+//     // console.log("hey")
 
-    let allTracks = {};
+//     let allTracks = {};
 
-    // concurrent GET requests using axios for each endpoint 
-    const getTracks = async () => {
+//     // concurrent GET requests using axios for each endpoint 
+//     const getTracks = async () => {
       
-      const playlists = await axios.all(playlistEndpoints.map((endpoint) => axios.get(endpoint)))
+//       const playlists = await axios.all(playlistEndpoints.map((endpoint) => axios.get(endpoint)))
       
-      // console.log("SUP")
+//       // console.log("SUP")
 
-      // loop through each playlist
-      for (let playlist of playlists) {
+//       // loop through each playlist
+//       for (let playlist of playlists) {
 
-        // playlist.data.tracks.items = array of track objects
-        let trackObj = playlist.data.tracks.items; 
-        // console.log("YO")
+//         // playlist.data.tracks.items = array of track objects
+//         let trackObj = playlist.data.tracks.items; 
+//         // console.log("YO")
 
-        for (const song of trackObj) {
-          if (song.track.id in allTracks) {
-            allTracks[`${song.track.id}`][1] += 1
-          } else {
-            allTracks[`${song.track.id}`] = [song.track.name, 1]
-          }
-        }
-      }
+//         for (const song of trackObj) {
+//           if (song.track.id in allTracks) {
+//             allTracks[`${song.track.id}`][1] += 1
+//           } else {
+//             allTracks[`${song.track.id}`] = [song.track.name, 1]
+//           }
+//         }
+//       }
 
-      // console.log("FUCK")
+//       // console.log("FUCK")
       
-      const allTracksArr = Object.values(allTracks);
-      const sortedTracks = allTracksArr.sort((a, b) => b[1] - a[1]);
+//       const allTracksArr = Object.values(allTracks);
+//       const sortedTracks = allTracksArr.sort((a, b) => b[1] - a[1]);
 
-      setTopSongs(sortedTracks.slice(0,20));
-    }
+//       setTopSongs(sortedTracks.slice(0,20));
+//     }
 
-    // console.log("SHIT")
+//     // console.log("SHIT")
 
-    catchErrors(getTracks())
+//     catchErrors(getTracks())
 
-    getTracks();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+//     getTracks();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
 
-  // console.log("UR MOM")
+//   // console.log("UR MOM")
 
-  return (
-    <div>
-      <ol>
-        {playlists ? topSongs.map((topSong, index) => {
-          return <li key={index}>{topSong[0]}: {topSong[1]}</li>
-        }): []}
-      </ol>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <ol>
+//         {playlists ? topSongs.map((topSong, index) => {
+//           return <li key={index}>{topSong[0]}: {topSong[1]}</li>
+//         }): []}
+//       </ol>
+//     </div>
+//   )
+// }
 
-export default TopSongs;
+// export default TopSongs;
