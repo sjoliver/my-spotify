678fb9c23889d386cc2161d620d547a15aa40ab1
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-28 16:28:40 -0700

    add handleSubmit fn -- makes concurrent axios get requests using the selected playlists' endpoints, sets the topSongs list

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import TopSongs from './TopSongs';
+// import TopSongs from './TopSongs';
 import { catchErrors } from '../utils';
 import { getCurrentUserPlaylists } from '../spotify';
 
@@ -8,6 +9,7 @@ const Playlists = () => {
   const [playlists, setPlaylists] = useState();
   const [checkedState, setCheckedState] = useState(new Array(20).fill(false));
   const [checkedPlaylists, setCheckedPlaylists] = useState({});
+  const [topSongs, setTopSongs] = useState([]);
 
   useEffect(() => {
     // getCurrentUserPlaylists returns a promise, we must use await to wait for the promise to be resolved -- we handle this by creating an async fn 
@@ -48,11 +50,46 @@ const Playlists = () => {
     }
   }
 
-  console.log("###",checkedPlaylists)
+  let playlistIds = Object.values(checkedPlaylists)
+  let playlistEndpoints = playlistIds.map(id => `/playlists/${id}`);
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
+    let allTracks = {};
+
+    // concurrent GET requests using axios for each endpoint 
+    const getTracks = async () => {
+      
+      const playlists = await axios.all(playlistEndpoints.map((endpoint) => axios.get(endpoint)))
+      
+      // loop through each playlist
+      for (let playlist of playlists) {
+
+        // playlist.data.tracks.items = array of track objects
+        let trackObj = playlist.data.tracks.items; 
+
+        for (const song of trackObj) {
+          if (song.track.id in allTracks) {
+            allTracks[`${song.track.id}`][1] += 1
+          } else {
+            allTracks[`${song.track.id}`] = [song.track.name, 1]
+          }
+        }
+      }
+      
+      const allTracksArr = Object.values(allTracks);
+      const sortedTracks = allTracksArr.sort((a, b) => b[1] - a[1]);
+
+      setTopSongs(sortedTracks.slice(0,20));
+    }
+
+    catchErrors(getTracks())
+
+    getTracks();
   }
+
+  console.log("**", topSongs)
 
   return (
     <>
@@ -81,7 +118,13 @@ const Playlists = () => {
           <button className='submit-btn' type='submit'>Submit</button>
         </div>
       </form>
-      <TopSongs playlists={playlists} />
+      <div>
+      <ol>
+        {playlists ? topSongs.map((topSong, index) => {
+          return <li key={index}>{topSong[0]}: {topSong[1]}</li>
+        }): []}
+      </ol>
+    </div>
     </>
   )
 }
