5be180799c0d8b69fc7335533f3acdb25794c380
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-05-30 15:11:40 -0700

    bug fix - trying to fix topSongs algo - WIP

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 // import TopSongs from './TopSongs';
 import { catchErrors } from '../utils';
-import { getCurrentUserPlaylists } from '../spotify';
+import { getCurrentUserPlaylists, getTestPlaylists } from '../spotify';
 
 const Playlists = () => {
   const [playlists, setPlaylists] = useState();
+  const [test, setTest] = useState();
   const [checkedState, setCheckedState] = useState(new Array(20).fill(false));
   const [checkedPlaylists, setCheckedPlaylists] = useState({});
   const [topSongs, setTopSongs] = useState([]);
@@ -19,14 +20,25 @@ const Playlists = () => {
       setPlaylists(data);
     };
 
+    const testData = async () => {
+      const { data } = await getTestPlaylists();
+
+      setTest(data);
+    }
+
     fetchData();
+
+    testData();
 
     // handles errors from async fn 
     catchErrors(fetchData());
+    catchErrors(testData());
 
   }, []);
 
   const handleChange = (position, id) => {
+
+    // when a box is a checked, a new array for state is created
     const updatedCheckedState = checkedState.map((item, index) => 
       index === position ? !item : item
     );
@@ -50,6 +62,7 @@ const Playlists = () => {
     }
   }
 
+  // create array of playlist IDs > use playlist ID array to create endpoint array 
   let playlistIds = Object.values(checkedPlaylists)
   let playlistEndpoints = playlistIds.map(id => `/playlists/${id}`);
 
@@ -61,7 +74,27 @@ const Playlists = () => {
     // concurrent GET requests using axios for each endpoint 
     const getTracks = async () => {
       
+      // get playlists
+      // playlists = array of playlist objects
       const playlists = await axios.all(playlistEndpoints.map((endpoint) => axios.get(endpoint)))
+
+      console.log("playlists", playlists)
+
+  
+      // put playlist songs into array
+
+      // count duplicates 
+
+      const playlistArr = [];
+
+      for (let playlist of playlists) {
+
+        playlistArr.push(playlist.data.tracks.items)
+
+      }
+
+
+
       
       // loop through each playlist
       for (let playlist of playlists) {
@@ -70,13 +103,15 @@ const Playlists = () => {
         let trackObj = playlist.data.tracks.items; 
 
         for (const song of trackObj) {
-          if (song.track.id in allTracks) {
-            allTracks[`${song.track.id}`][1] += 1
-          } else {
+          if (allTracks[song.track.id] === undefined) {
             allTracks[`${song.track.id}`] = [song.track.name, 1]
+          } else {
+            allTracks[`${song.track.id}`][1] += 1
           }
         }
       }
+
+      console.log("**", allTracks)
       
       const allTracksArr = Object.values(allTracks);
       const sortedTracks = allTracksArr.sort((a, b) => b[1] - a[1]);
@@ -88,8 +123,6 @@ const Playlists = () => {
 
     getTracks();
   }
-
-  console.log("**", topSongs)
 
   return (
     <>
