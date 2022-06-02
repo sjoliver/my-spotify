9f87e8d2acae75f8876d7e1dcd81dc7c7d713e7c
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-06-01 17:36:03 -0700

    change top 20 to 10, add classNames for styling, add titles

--- client/src/pages/Playlists.js
+++ client/src/pages/Playlists.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 import { catchErrors } from '../utils';
 import { getCurrentUserPlaylists } from '../spotify';
 
+import '../styles/Playlists.css'
+
 const Playlists = () => {
   const [playlists, setPlaylists] = useState();
   const [checkedState, setCheckedState] = useState(new Array(20).fill(false));
   const [checkedPlaylists, setCheckedPlaylists] = useState({});
-  const [topSongs, setTopSongs] = useState([]);
+  const [topSongs, setTopSongs] = useState();
 
   useEffect(() => {
     // getCurrentUserPlaylists returns a promise, we must use await to wait for the promise to be resolved -- we handle this by creating an async fn 
@@ -90,45 +92,54 @@ const Playlists = () => {
       const sortedTracks = allTracksArr.sort((a, b) => b[1] - a[1]);
   
       // set top songs state array and slice it at 20 element
-      setTopSongs(sortedTracks.slice(0,20));
+      setTopSongs(sortedTracks.slice(0,10));
     }
     catchErrors(getTracks());
   }
 
   return (
     <>
-      <h3>Select Playlists</h3>
-      <form onSubmit={handleSubmit}>
-        <ul className='playlists-list' style={{"listStyle": "none"}}>
-          {playlists?.items.map((playlist, index) => {
-            return (
-              <li key={playlist + index}>
-                <div className='playlists-list-item'>
-                  <input 
-                    type='checkbox'
-                    id={`custom-checkbox-${index}`}
-                    name={playlist.name}
-                    value={playlist.name}
-                    checked={checkedState[index]}
-                    onChange={() => handleChange(index, playlist.id)}
-                  />
-                  <label htmlFor={`custom-checkbox-${index}`}>{playlist.name}</label>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-        <div>
-          <button className='submit-btn' type='submit'>Submit</button>
+      <h2 id='page-title'>Playlist Analysis</h2>
+      <div className='content-container'>
+        <div className='form-wrapper'>
+          <h2 className='titles'>Select Playlists</h2>
+          <form onSubmit={handleSubmit} className='form'>
+            <ul className='playlists-list'>
+              {playlists?.items.map((playlist, index) => {
+                return (
+                  <li key={playlist + index} className='playlists-list-item'>
+                    <input 
+                      type='checkbox'
+                      className='checkbox-item'
+                      id={`custom-checkbox-${index}`}
+                      name={playlist.name}
+                      value={playlist.name}
+                      checked={checkedState[index]}
+                      onChange={() => handleChange(index, playlist.id)}
+                    />
+                    <label htmlFor={`custom-checkbox-${index}`}>{playlist.name}</label>
+                  </li>
+                )
+              })}
+            </ul>
+            <button className='submit-btn' type='submit'>Analyze</button>
+          </form>
         </div>
-      </form>
-      <div>
-      <ol>
-        {playlists ? topSongs.map((topSong, index) => {
-          return <li key={topSong+index}>{topSong[0]}: {topSong[1]}</li>
-        }): []}
-      </ol>
-    </div>
+        {topSongs && 
+          <div className='topsongs-list-wrapper'>
+            <h2 className='titles'>Top Songs</h2>
+            <ol className='topsongs-list'>
+              {topSongs?.map((topSong, index) => {
+                return (
+                  <li key={topSong+index} className='topsongs-list-item'>
+                    {topSong[0]}: {topSong[1]}
+                  </li>
+                )
+              })}
+            </ol>
+          </div>
+        }
+      </div>
     </>
   )
 }
