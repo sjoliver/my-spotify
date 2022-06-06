e4219a325e348d5036611ae4e01296d41eeb18f5
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-06-06 14:17:51 -0700

    rearrange elements, add classNames

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
 import { getCurrentUserProfile } from '../spotify';
+import '../styles/Profile.css'
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
 
-  console.log(profile)
+  // console.log(profile.images)
 
   useEffect(() => {
     // getCurrentUserProfile returns a promise, we must use await to wait for the promise to be resoled -- we handle this by creating an async fn 
@@ -23,13 +24,17 @@ const Profile = () => {
   return (
     <>
       {profile && (
-        <div>
-          <h1>{profile.display_name}</h1>
-          <p>{profile.followers.total} Followers</p>
-          <a href="/playlists">Playlists</a>
+        <div className='profile-wrapper'>
           {profile.images.length && profile.images[0].url && (
-            <img src={profile.images[0].url} alt="Avatar"/>
+            <div className='profile-img-wrapper'>
+              <img className='profile-img' src={profile.images[0].url} alt="Avatar"/>
+            </div>
           )}
+          <div>
+            <h1>{profile.display_name}</h1>
+            <p>{profile.followers.total} Followers</p>
+          </div>
+          <a href="/playlists">Playlists</a>
         </div>
       )}
     </>
