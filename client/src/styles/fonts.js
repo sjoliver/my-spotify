e7229fb612b6ab9f08ab09988352b1278ba610e7
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-06-10 13:31:01 -0700

    change global font to better match app title

--- client/src/styles/fonts.js
+++ client/src/styles/fonts.js
@@ -1,7 +1,9 @@
 import { css } from 'styled-components/macro';
 
 const fonts = css`
-  font-family: sans-serif;
+  font-family: 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
   font-weight: 400;
   font-style: normal;
 `;
