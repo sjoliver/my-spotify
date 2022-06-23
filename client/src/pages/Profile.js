2df73e18be1eaeab4d7968648aa3638881050354
Sophie Oliver <sophiejoliver19+1@gmail.com>
2022-06-22 18:40:52 -0700

    move fetchData fn declaration outside useEffect, add conditional for displaying a users avatar -- display an icon if user has no avatar

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
 import { getCurrentUserProfile } from '../spotify';
 import '../styles/Profile.css'
+import { SiEgghead } from 'react-icons/si'
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
 
-  useEffect(() => {
     // getCurrentUserProfile returns a promise, we must use await to wait for the promise to be resoled -- we handle this by creating an async fn 
-    const fetchData = async () => {
-      const { data } = await getCurrentUserProfile();
+  const fetchData = async () => {
+    const { data } = await getCurrentUserProfile();
+    // set the state variable with the response from the axios.get('/me')
+    setProfile(data);
+  };
 
-      // set the state variable with the response from the axios.get('/me')
-      setProfile(data);
-    };
+  useEffect(() => {
+    fetchData();
 
     // handles errors from async fn 
     catchErrors(fetchData());
@@ -26,11 +28,15 @@ const Profile = () => {
           <div className='name-followers'>
             <p id='display-name'>{profile.display_name}</p>
           </div>
-          {profile.images.length && profile.images[0].url && (
+          {profile.images[0] ? 
             <div className='profile-img-wrapper'>
               <img className='profile-img' src={profile.images[0].url} alt="Avatar"/>
             </div>
-          )}
+            :
+            <div className='egghead-wrapper'>
+              <SiEgghead size={28}/>
+            </div>
+          }
         </div>
       )}
     </>
